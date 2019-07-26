586953dbdc1d93b23d3fb1d1525b02612166d22c
cyrus <cyruscyliu@gmail.com>
2019-07-26 14:17:42 +0800

    fix ARMCPU with * and change object_init_child to ARM_CPU(object_new...

--- generation/template/soc_name.c
+++ generation/template/soc_name.c
@@ -16,8 +16,7 @@ static void {{soc_name}}_init(Object *obj) {
 
     /* initialize cpus and add the cpu as soc's child */
     s->cpu_type = ARM_CPU_TYPE_NAME("{{cpu_type}}");
-    object_initialize_child(
-        obj, "cpu", &s->cpu,  sizeof(s->cpu), s->cpu_type, &error_abort, NULL);
+    s->cpu = ARM_CPU(object_new(machine->cpu_type));
 
     /* initialize the interrupt controller and add the ic as soc and sysbus's child*/
     sysbus_init_child_obj(
--- generation/wrt350n_v2/mv88f5181L.c
+++ generation/wrt350n_v2/mv88f5181L.c
@@ -18,8 +18,7 @@ static void mv88f5181L_init(Object *obj) {
 
     /* initialize cpus and add the cpu as soc's child */
     s->cpu_type = ARM_CPU_TYPE_NAME("arm926");
-    object_initialize_child(
-        obj, "cpu", &s->cpu,  sizeof(s->cpu), s->cpu_type, &error_abort, NULL);
+    s->cpu = ARM_CPU(object_new(machine->cpu_type));
 
     /* initialize the interrupt controller and add the ic as soc and sysbus's child*/
     sysbus_init_child_obj(
