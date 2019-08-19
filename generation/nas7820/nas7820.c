48b4f9ed33dec452361a4e1e12c4e46d8fdbc7b3
cyrus <cyruscyliu@gmail.com>
2019-08-19 08:53:57 +0800

    the ram size must larger than kernel load addr 0x60008000

--- generation/nas7820/nas7820.c
+++ generation/nas7820/nas7820.c
@@ -103,7 +103,7 @@ static void nas7820_machine_init(MachineClass *mc)
     /* mc->default_display = ; */
     /* mc->compat_props = ; */
     /* mc->hw_version = ; */
-    mc->default_ram_size = 256 * MiB; /* 1 * GiB[MiB], 1024 * 1024 [* 1024] */
+    mc->default_ram_size = 2 * GiB; /* 1 * GiB[MiB], 1024 * 1024 [* 1024] */
     mc->default_cpu_type = ARM_CPU_TYPE_NAME("arm11mpcore");
     /* mc->default_kernel_irqchip_split = ; */
     /* mc->option_rom_has_mr = ; */
