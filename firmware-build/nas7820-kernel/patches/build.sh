eb1ee98585fddf6c39e61ec27e4d3e737eaf78d2
cyrus <cyruscyliu@gmail.com>
2019-08-28 13:18:11 +0800

    update more invalid urls

--- firmware-build/nas7820-kernel/patches/build.sh
+++ firmware-build/nas7820-kernel/patches/build.sh
@@ -5,4 +5,5 @@ target="./chaos_camler_1505_oxnas"
 cd $target
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+# cp feeds_packages_utils_bc.Makefile ./feeds/packages/utils/bc/Makefile
 make -j4 V=s
