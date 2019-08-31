5b85b82ce0fabe5999048a9e621089eb80d49899
cyrus <cyruscyliu@gmail.com>
2019-08-31 10:04:55 +0800

    remove feeds update and install

--- firmware-build/nas7820-kernel/patches/build.sh
+++ firmware-build/nas7820-kernel/patches/build.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# update the packages
 target="./chaos_camler_1505_oxnas"
 cd $target
-./scripts/feeds update -a
-./scripts/feeds install -a
-# cp feeds_packages_utils_bc.Makefile ./feeds/packages/utils/bc/Makefile
 make -j4 V=s
