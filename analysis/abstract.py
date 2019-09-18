d622801a5e8b2439b8e9ba9de3636ffbca31bba2
cyrus <cyruscyliu@gmail.com>
2019-09-18 10:55:14 +0800

    improve binwalk output format

--- analysis/abstract.py
+++ analysis/abstract.py
@@ -14,10 +14,22 @@ def by_binwalk(firmware):
     The patched binwalk must be available.
     """
     logger.info('extract kernel and dtb by binwalk')
-    for module in binwalk.scan(firmware.working_path, signature=True, quiet=True, block=firmware.size):
+    for module in binwalk.scan(firmware.working_path, signature=True,
+                               quiet=True, block=firmware.size):
+        count = 0
         for result in module.results:
-            print("\t%s    0x%.8X    %s [%s]" % (
+            if str(result.description).find('flattened image tree') != -1:
+                logger.info('\033[32mFIT uImage found\033[0m, offset {}, {}'.format(result.offset, result.description))
+                count += 1
+            elif str(result.description).find('uImage') != -1:
+                logger.info('\033[32mLegacy uImage found\033[0m, offset {}, {}'.format(result.offset, result.description))
+                count += 1
+            else:
+                logger.debug("\t%s    0x%.8X    %s [%s]" % (
                 result.file.name, result.offset, result.description, str(result.valid)))
+
+        if not count:
+            logger.info('no support for other firmware header')
 
 
 def register_extract_kernel_and_dtb(func):
