18ea23a2bf2e38a65ff385614d0466282228e1d9
cyrus <cyruscyliu@gmail.com>
2019-10-11 23:26:47 +0800

    update api and logging used in get_source_code

--- analyses/srcode.py
+++ analyses/srcode.py
@@ -5,39 +5,30 @@ import os
 import yaml
 import logging
 
+from manager import finished, finish
+
 logger = logging.getLogger()
-
-__process_source_code = []
-
-
-def register_process_source_code(func):
-    __process_source_code.append(func)
-
-
-def process_source_code(firmware):
-    for func in __process_source_code:
-        func(firmware)
+TASK_DESCRIPTION = 'let\'s get its source code'
 
 
 def get_source_code(firmware):
-    brand = firmware.get('brand')
+    if finished(firmware, 'get_source_code', 'by_default'):
+        return
+    brand = firmware.get_brand()
     if brand == 'openwrt':
-        logger.info('get source code by openwrt table of hardware')
-        if firmware.get('toh') is None:
-            if firmware.get('target') is None or \
-                    firmware.get('subtarget') is None:
-                raise ValueError('incomplete information for finding source code')
-        with open(os.path.join(os.getcwd(), 'database', 'openwrt.yaml')) as f:
-            openwrt_release_info = yaml.safe_load(f)
-        try:
-            url = openwrt_release_info[firmware.get('revision')]['url']
-        except KeyError:
-            logger.info('no available url for this version {}'.format(firmware.get('revision')))
-            return
-        possible_package = os.path.join(
-            url,
-            firmware.get('target'),  # target
-            firmware.get('subtarget'),  # subtarget
-        )
-        logger.info(
-            '\033[32mdownload page found for OpenWrt project {}\033[0m'.format(possible_package))
+        if firmware.homepage is None:
+            target = firmware.get_target()
+            subtarget = firmware.get_subtarget()
+
+            with open(os.path.join(os.getcwd(), 'database', 'openwrt.yaml')) as f:
+                openwrt_release_info = yaml.safe_load(f)
+            try:
+                url = openwrt_release_info[firmware.get_revision()]['url']
+                possible_package = os.path.join(url, target, subtarget)
+                logger.info(
+                    '\033[32mdownload page found for OpenWrt project {}\033[0m'.format(
+                        possible_package))
+            except NotImplementedError:
+                logger.info('no available url for this version {}'.format(firmware.get('revision')))
+                return
+    finish(firmware, 'get_source_code', 'by_default')
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from analyses.flash import get_flash_info
 from analyses.ic import get_ic_info
 from analyses.metadata import get_metadata
 from analyses.ram import get_ram_info
+from analyses.srcode import get_source_code
 from analyses.timer import get_timer_info
 from analyses.uart import get_uart_info
 
@@ -31,7 +32,7 @@ def analysis(firmware):
     # let's start
     extract_kernel_and_dtb(firmware)
     get_metadata(firmware)
-    # get_source_code(firmware)
+    get_source_code(firmware)
     get_cpu_model_info(firmware)
     get_ram_info(firmware)
     get_flash_info(firmware)
