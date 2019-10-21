e7db15602a76cf832b9537db9e56eed8931b661f
cyrus <cyruscyliu@gmail.com>
2019-10-21 12:43:18 +0800

    add more tests

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
 python3.7 -m unittest tests.test_generation.TestGeneration.test_nas7820
 python3.7 -m unittest tests.test_generation.TestGeneration.test_wrt320n_n1
 python3.7 -m unittest tests.test_generation.TestGeneration.test_wrt350n_v2
+python3.7 main.py -dbt firmadyne -p dt -u 13882 -r -wd ./build
+python3.7 main.py -dbt firmadyne -p dt -u 14292 -r -wd ./build
+python3.7 main.py -dbt firmadyne -p dt -u 15007 -r -wd ./build
