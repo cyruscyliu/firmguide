f1ed0d356de00bb16ab75702ad9d628a869eb592
cyruscyliu <cyruscyliu@gmail.com>
2020-02-08 13:26:33 +0000

    pretend to be ready for rootfs

--- analyses/preparation.py
+++ analyses/preparation.py
@@ -50,7 +50,7 @@ class Preparation(Analysis):
 
         # 4. prepare -initrd path/to/cpio
         path_to_initramfs = \
-            os.path.join(BASE_DIR, 'examples/rootfs/{}.cpio'.format(firmware.get_machine_name()))
+            os.path.join(BASE_DIR, 'examples/rootfs/{}.cpio.rootfs'.format(firmware.get_arch()))
         path_to_initramfs = pack_initramfs(
             firmware.get_components(), mounted_to=path_to_initramfs)
         running_command = firmware.qemuc.get_command(
