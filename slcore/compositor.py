f808c855ddd6c0f248a0fe48fb3c1e4600e19a2e
Qiang Liu <qiangliu@zju.edu.cn>
2020-03-24 05:51:56 +0000

    fix bug in ubidump

--- slcore/compositor.py
+++ slcore/compositor.py
@@ -352,7 +352,8 @@ def unpack(path, target_dir=None, extract=True):
                 # we have a correct peb size
                 path_to_raw = components.get_path_to_raw()
                 path_to_image = module.extractor.output[result.file.path].carved[v1]
-                output = ubidump(path_to_image, result.peb_size, result.data_offset)
+                print(dir(result), result.jump)
+                output = ubidump(path_to_image, result.jump, result.data_offset)
                 components = unpack(output, target_dir=os.path.dirname(output))
                 components.set_type(UBI_KERNEL)
                 components.set_path_to_raw(path_to_raw)
