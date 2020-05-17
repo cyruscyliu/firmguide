829c617920ae289b0e1649ff814171fc39c1caa3
Qiang <liuqiang@zju.edu.cn>
2020-05-17 05:33:00 +0000

    update test qemuc

--- tests/test_compositor.py
+++ tests/test_compositor.py
@@ -1,11 +1,10 @@
 from unittest import TestCase
-
 from slcore.compositor import unpack
 from slcore.compositor import Components
-from settings import *
 
 import os
 import tempfile
+
 
 class TestCompositor(TestCase):
     def test_dynamic_attrs(self):
@@ -13,10 +12,10 @@ class TestCompositor(TestCase):
 
         self.assertEqual(None, components.get_path_to_kernel())
 
-
     def test_unpack2(self):
         components = unpack(
-            os.path.join(TESTS_DIR, '2b38a390ba53209a1fa4c6aed8489c14774db4c9.bin'),
+            os.path.join(os.path.dirname(__file__),
+                         '2b38a390ba53209a1fa4c6aed8489c14774db4c9.bin'),
             target_dir=tempfile.gettempdir(), extract=False)
 
         self.assertIsNotNone(components.get_path_to_uimage())
@@ -27,7 +26,8 @@ class TestCompositor(TestCase):
 
     def test_unpack3(self):
         components = unpack(
-            os.path.join(TESTS_DIR, 'ec5859077831e078987ebb05461d4ec834896f3e.bin'),
+            os.path.join(os.path.dirname(__file__),
+                         'ec5859077831e078987ebb05461d4ec834896f3e.bin'),
             target_dir=tempfile.gettempdir(), extract=False)
 
         self.assertIsNotNone(components.get_path_to_uimage())
@@ -37,9 +37,8 @@ class TestCompositor(TestCase):
 
     def test_unpack4(self):
         components = unpack(
-            os.path.join(TESTS_DIR, '00107b9211802ca6923ab9bdcdc891266562db70.bin'),
+            os.path.join(os.path.dirname(__file__),
+                         '00107b9211802ca6923ab9bdcdc891266562db70.bin'),
             target_dir=tempfile.gettempdir(), extract=False)
 
         self.assertFalse(components.supported)
-
-
--- tests/test_qemuc.py
+++ tests/test_qemuc.py
@@ -1,25 +1,30 @@
 from unittest import TestCase
-
-from slcore.qemuc import QEMUController
-from settings import *
+from slcore.qemucontroller import QEMUController
 
 import os
 
+QEMU_DIR = os.path.join(os.path.dirname(__file__), 'qemu-4.0.0')
+
+
 class TestQEMUController(TestCase):
     def test_get_command(self):
-        qemuc = QEMUController(os.path.join(WORKING_DIR, QEMU_DIR_NAME))
-        path_to_vmlinux = os.path.join(TESTS_DIR, 'vmlinux')
-        running_command = qemuc.get_command('arm', 'l', 'wrt350n_v2', path_to_vmlinux)
-        # qemu-system-arm -M wrt350n_v2 -kernel /root/esv/tests/vmlinux -nographic -append "console=ttyS0"
+        qemuc = QEMUController(QEMU_DIR)
+        path_to_vmlinux = os.path.join(os.path.dirname(__file__), 'vmlinux')
+        running_command = \
+            qemuc.get_command('arm', 'l', 'wrt350n_v2', path_to_vmlinux)
+        # qemu-system-arm -M wrt350n_v2
+        # -kernel /root/esv/tests/vmlinux -nographic -append "console=ttyS0"
+        print(running_command)
         self.assertEqual(
             running_command.split('-M')[1].strip(),
-            'wrt350n_v2 -kernel /root/esv/tests/vmlinux -nographic -append "console=ttyS0"')
+            'wrt350n_v2 -kernel {} -nographic -append "console=ttyS0 nowatchdog nokaslr"'.format(
+                path_to_vmlinux))
 
     def test_patch_recover(self):
-        qemuc = QEMUController(os.path.join(WORKING_DIR, QEMU_DIR_NAME))
+        qemuc = QEMUController(QEMU_DIR)
 
         # patch(abs/p/t/old, rel/p/t/new, bak=True)
-        old = os.path.join(BASE_DIR, 'examples/machines/wrt350n_v2.c')
+        old = os.path.join(os.path.dirname(__file__), 'wrt350n_v2.c')
         new = 'hw/arm/wrt350n_v2.c'
 
         # 1 patch a file which doesn't exist
@@ -28,7 +33,7 @@ class TestQEMUController(TestCase):
         self.assertFalse(os.path.exists(new))
 
         # 2 patch a file which exists
-        abs_new = os.path.join(WORKING_DIR, QEMU_DIR_NAME, new)
+        abs_new = os.path.join(QEMU_DIR, new)
         os.system('cp {} {}'.format(old, abs_new))
         qemuc.patch(old, new)
         self.assertTrue(os.path.exists(abs_new + '.bak'))
@@ -37,24 +42,26 @@ class TestQEMUController(TestCase):
         os.system('rm {}'.format(abs_new))
 
     def test_install_recover(self):
-        qemuc = QEMUController(os.path.join(WORKING_DIR, QEMU_DIR_NAME))
+        qemuc = QEMUController(QEMU_DIR)
 
         # install(prefix)
-        prefix = os.path.join(TESTS_DIR, 'qemu_install')
+        prefix = \
+            os.path.join(os.path.dirname(__file__), 'qemu-install')
         qemuc.install(prefix)
-        abs_new = os.path.join(WORKING_DIR, QEMU_DIR_NAME, 'wrt350n_v2.c')
+        abs_new = os.path.join(QEMU_DIR, 'wrt350n_v2.c')
         self.assertTrue(os.path.exists(abs_new))
         qemuc.recover()
         self.assertFalse(os.path.exists(abs_new))
 
     def test_compile(self):
-        qemuc = QEMUController(os.path.join(WORKING_DIR, QEMU_DIR_NAME))
+        qemuc = QEMUController(QEMU_DIR)
         qemuc.compile()
 
     def test_debug(self):
-        qemuc = QEMUController(os.path.join(WORKING_DIR, QEMU_DIR_NAME))
-        path_to_vmlinux = os.path.join(TESTS_DIR, 'vmlinux')
-        running_command = qemuc.get_command('arm', 'l', 'versatilepb', path_to_vmlinux)
+        qemuc = QEMUController(QEMU_DIR)
+        path_to_vmlinux = os.path.join(os.path.dirname(__file__), 'vmlinux')
+        running_command = \
+            qemuc.get_command('arm', 'l', 'versatilepb', path_to_vmlinux)
         qemuc.debug(running_command, path_to_vmlinux)
 
     def test_trace(self):
@@ -62,16 +69,14 @@ class TestQEMUController(TestCase):
 
     def test_add_target(self):
         # add_target('hwname', 'hwname', type_='hw', arch='arm', endian='l')
-        qemuc = QEMUController(os.path.join(WORKING_DIR, QEMU_DIR_NAME))
+        qemuc = QEMUController(QEMU_DIR)
         qemuc.add_target('hwname', 'hwname', t='hw', arch='arm', endian='l')
         qemuc.recover()
 
         # add_target('hwname', 'fname', type_='intc')
         qemuc.add_target('hwname', 'fname', t='intc')
-        abs_new = os.path.join(WORKING_DIR, QEMU_DIR_NAME, 'hw/intc/Makefile.objs')
+        abs_new = os.path.join(QEMU_DIR, 'hw/intc/Makefile.objs')
         self.assertTrue(os.path.exists(abs_new + '.bak'))
         qemuc.recover()
         self.assertTrue(os.path.exists(abs_new))
         self.assertFalse(os.path.exists(abs_new + '.bak'))
-
-
