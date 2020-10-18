b70cfacd2d8d2d9df8b12c4586fff74484749686
Qiang Liu <cyruscyliu@gmail.com>
2020-10-18 05:48:06 +0000

    update archive

--- slcore/analyses/archive.py
+++ slcore/analyses/archive.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 
 from slcore.amanager import Analysis
 from slcore.database.dbf import get_database
@@ -17,4 +18,20 @@ class Archive(Analysis):
         """
         Must be called after ./firmware systhesize ...
         """
+        profile = kwargs.pop('profile')
+        if profile is None:
+            profile = '.profile'
+
+        machine_name = yaml.safe_load(open(profile))['basics']['machine_name']
+
+        # Copy profile to database
+        base_dir = self.analysis_manager.project.attrs['base_dir']
+        target = os.path.join(
+            base_dir, 'slcore/database/by_compatible',
+            machine_name + '.yaml')
+        os.system('cp {} {}'.format(profile, target))
+        self.info('archive {}'.format(target), 1)
+
+        # TODO
+        # Update record in some summary file
         return True
