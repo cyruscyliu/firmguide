374076246cf4f9c8c5a5b768cd0e609b4f7702e6
Qiang Liu <cyruscyliu@gmail.com>
2023-05-28 18:25:12 -0700

    add some supported arm kernels
    Signed-off-by: Qiang Liu <cyruscyliu@gmail.com>

---
+++ examples/more_images_bootable.sh
@@ -0,0 +1,8 @@
+wget https://archive.openwrt.org/chaos_calmer/15.05.1/bcm53xx/generic/openwrt-15.05.1-bcm53xx-netgear-r6250-squashfs.chk
+wget https://archive.openwrt.org/chaos_calmer/15.05.1/bcm53xx/generic/openwrt-15.05.1-bcm53xx-netgear-r6300-v2-squashfs.chk
+wget https://archive.openwrt.org/chaos_calmer/15.05.1/bcm53xx/generic/openwrt-15.05.1-bcm53xx-netgear-r7000-squashfs.chk
+wget https://archive.openwrt.org/chaos_calmer/15.05.1/bcm53xx/generic/openwrt-15.05.1-bcm53xx-netgear-r8000-squashfs.chk
+wget https://archive.openwrt.org/chaos_calmer/15.05.1/bcm53xx/generic/openwrt-15.05.1-bcm53xx-smartrg-sr400ac-squashfs.trx
+wget https://archive.openwrt.org/chaos_calmer/15.05.1/bcm53xx/generic/openwrt-15.05.1-bcm53xx-smartrg-sr400ac-squashfs.trx
+wget https://archive.openwrt.org/chaos_calmer/15.05.1/kirkwood/generic/openwrt-15.05.1-kirkwood-ea3500-uImage
+wget https://archive.openwrt.org/chaos_calmer/15.05.1/kirkwood/generic/openwrt-15.05.1-kirkwood-ea4500-uImage
